d461a2f3d37eb2ae10f5d408d19dbee924011841
Lochlan Bunn <loklaan@gmail.com>
2014-09-07 03:30:33 +1000

    Removed lang services. Added stop word matching for content.

--- script.js
+++ script.js
@@ -28,10 +28,7 @@
     isShowing = false;
   };
 
-  Exports.buildStems = function(language) {
-    if (!language) {
-      language = 'EN';
-    }
+  Exports.buildStems = function() {
 
     var steps = {
       one: $('#step1').prop('checked'),
@@ -45,13 +42,16 @@
     stemmed = [];
     diffed = [];
     var diffcount= 0,
-    timer = 0;
+    timer = 0,
+    lang;
     var clean = $('#input').val();
     if (typeof clean === "undefined") {
       return;
     }
-    clean = clean.replace(/[^\w]/g, ' ').replace(/\s+/g, ' ');
-    getLanguage(clean);
+
+    clean = clean.replace(/'/g, '').replace(/[^\w]/g, ' ').replace(/\s+/g, ' ');
+    lang = getLanguage(clean);
+    $('#language').text(lang);
 
     wordlist = clean.split(' ');
 
@@ -141,20 +141,20 @@
   }
 
   function getLanguage(words) {
-    var translateUrl = "http://ws.detectlanguage.com/0.2/detect?key=6c1e3bd21fe57a4007a8784cf0a95edc&q=";
+    var langs = {
+      fre: 'French',
+      en: 'English'
+    };
+    var frenchResults = words.match(frenchStopWords);
+    var englishResults = words.match(englishStopWords);
 
-    var jqxhr = $.ajax(translateUrl + encodeURI(words).substr(0, 99))
-      .done(function(res) {
-        console.log(res);
-      })
-      .fail(function(res) {
-        console.log(res);
-
-      })
-      .always(function(res) {
-        console.log(res);
-
-      });
+    if (frenchResults === null) {
+      return langs.en;
+    } else if (englishResults === null) {
+      return langs.fre;
+    } else {
+      return frenchResults.length > englishResults.length ? langs.fre : langs.en ;
+    }
   }
 
 /*
@@ -215,5 +215,9 @@
     val = val.split(' ').length;
     return val;
   }
+
+  var frenchStopWords = /(?:\s)(alors|au|aucuns|aussi|autre|avant|avec|avoir|bon|car|ce|cela|ces|ceux|chaque|ci|comme|comment|dans|des|du|dedans|dehors|depuis|deux|devrait|doit|donc|dos|droite|début|elle|elles|en|encore|essai|est|et|eu|fait|faites|fois|font|force|haut|hors|ici|il|ils|je|juste|la|le|les|leur|là|ma|maintenant|mais|mes|mine|moins|mon|mot|même|ni|nommés|notre|nous|nouveaux|ou|où|par|parce|parole|pas|personnes|peut|peu|pièce|plupart|pour|pourquoi|quand|que|quel|quelle|quelles|quels|qui|sa|sans|ses|seulement|si|sien|son|sont|sous|soyez||sujet|sur|ta|tandis|tellement|tels|tes|ton|tous|tout|trop|très|tu|valeur|voie|voient|vont|votre|vous|vu|ça|étaient|état|étions|été|être)(?:\s)/gi;
+
+  var englishStopWords = /(?:\s)(a|about|above|after|again|against|all|am|an|and|any|are|aren't|as|at|be|because|been|before|being|below|between|both|but|by|can't|cannot|could|couldn't|did|didn't|do|does|doesn't|doing|don't|down|during|each|few|for|from|further|had|hadn't|has|hasn't|have|haven't|having|he|he'd|he'll|he's|her|here|here's|hers|herself|him|himself|his|how|how's|i|i'd|i'll|i'm|i've|if|in|into|is|isn't|it|it's|its|itself|let's|me|more|most|mustn't|my|myself|no|nor|not|of|off|on|once|only|or|other|ought|our|ours||ourselves|out|over|own|same|shan't|she|she'd|she'll|she's|should|shouldn't|so|some|such|than|that|that's|the|their|theirs|them|themselves|then|there|there's|these|they|they'd|they'll|they're|they've|this|those|through|to|too|under|until|up|very|was|wasn't|we|we'd|we'll|we're|we've|were|weren't|what|what's|when|when's|where|where's|which|while|who|who's|whom|why|why's|with|won't|would|wouldn't|you|you'd|you'll|you're|you've|your|yours|yourself|yourselves)(?:\s)/gi;
 
 })(window, document);
